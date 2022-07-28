c37d3e66a7d2a5c41e5d50c1baa58ebcb7cea3fb
Chris Taylor <ctaylor534@gmail.com>
2022-07-28 10:31:02 -0700

    adds login view

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
     <router-link to="/sample">Sample</router-link>
     |
     <router-link to="/signup">Sign Up</router-link>
+    |
+    <router-link to="/login">Log In</router-link>
   </nav>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import SampleView from "../views/SampleView.vue";
 import SignupView from "../views/SignupView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
@@ -26,6 +27,11 @@ const routes = [
     path: "/signup",
     name: "signup",
     component: SignupView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
 ];
 
