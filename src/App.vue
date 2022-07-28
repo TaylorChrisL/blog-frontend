c9a038cc9d3e3435e0964f56917ca51e1d83555c
Chris Taylor <ctaylor534@gmail.com>
2022-07-28 10:25:29 -0700

    Adds new views

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <router-link to="/">Home</router-link>
     |
     <router-link to="/about">About</router-link>
+    |
+    <router-link to="/sample">Sample</router-link>
+    |
+    <router-link to="/signup">Sign Up</router-link>
   </nav>
   <router-view />
 </template>
