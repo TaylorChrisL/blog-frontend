16e19017b238de0455b0c512eae660e3a861a98e
Chris Taylor <ctaylor534@gmail.com>
2022-08-01 09:47:37 -0700

    Adds single post view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import PostsIndex from "../views/PostsIndex.vue";
 import PostsNew from "../views/PostsNew.vue";
+import PostsShow from "../views/PostsShow.vue";
 import SignupView from "../views/SignupView.vue";
 import LoginView from "../views/LoginView.vue";
 import LogoutView from "../views/LogoutView.vue";
@@ -21,6 +22,11 @@ const routes = [
     path: "/posts/new",
     name: "postsnew",
     component: PostsNew,
+  },
+  {
+    path: "/posts/1",
+    name: "posts-1",
+    component: PostsShow,
   },
   {
     path: "/signup",
---
+++ src/views/PostsShow.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      post: {},
+    };
+  },
+  created: function () {
+    axios.get("/posts/1.json").then((response) => {
+      this.post = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="posts-show">
+    <div class="container">
+      <h1>{{ post.title }}</h1>
+      <img :src="post.image" alt="An Image" />
+      <p>
+        {{ post.body }}
+      </p>
+      <router-link to="/posts">Return to All Posts</router-link>
+    </div>
+  </div>
+</template>
+
+<style>
+img {
+  max-width: 200px;
+  max-height: 300px;
+}
+</style>
